7293b1d437c2a6be6fa1c9117d628050bee09223
Alberto <42894888+Nenino2@users.noreply.github.com>
2023-12-31 18:42:05 +0100

    first commit

---
+++ src/main/java/com/alberto/myfirstspringwebapp/MyfirstspringwebappApplication.java
@@ -0,0 +1,13 @@
+package com.alberto.myfirstspringwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfirstspringwebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfirstspringwebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alberto/myfirstspringwebapp/MyfirstspringwebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alberto.myfirstspringwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfirstspringwebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
